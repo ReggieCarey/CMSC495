fcba5a5c9d3da484c1ce250c49ffcc83e1624f10
rcoolb2002 <rcoolb2001@yahoo.com>
2016-04-20 22:44:07 -1000

    Updated Currency
    Added SmallestDenomination handling.

--- Currency.java
+++ Currency.java
@@ -1,13 +1,62 @@
+
+import java.util.Arrays;
+import java.util.List;
+
+
+
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Class: Currency
+ * 
  */
 
-/**
- *
- * @author rcool
- */
+
 public class Currency {
+    private String name;
+    private String code;
+    private Rate rate;
+    private boolean smallestDenomination = false;
+    private static List noDecimalCodes = Arrays.asList("BIF","DJF","JPY",
+            "KRW","PYG","VND","XAF","XPF","CLP","GNF","KMF","MGA","RWF",
+            "VUV","XOF");
+
+
+    //Constructor for currency
+    public Currency(String name, String code, Rate rate)
+    {
+        this.name=name;
+        this.code = code;
+        this.rate.setRate(rate);
+        if(noDecimalCodes.contains(code))
+            smallestDenomination=true;
+    }
     
+    //Constructor for currency object for rate storage only
+    public Currency(Rate rate)
+    {
+        this.rate = rate;
+    }
+    
+    //Method to update currency rate given a Rate object
+    public void updateRate(Rate rate)
+    {
+        this.rate = rate;
+    }
+    
+    //Method to get currency name
+    public String getName()
+    {
+        return this.name;
+    }
+    
+    //Method to get currency code
+    public String getCode()
+    {
+        return this.code;
+    }
+    
+    //Method to get currency Rate
+    public Rate getRate()
+    {
+        return this.rate;
+    }
 }
--- Rate.java
+++ Rate.java
@@ -1,13 +1,49 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author rcool
- */
+import java.time.LocalDateTime;
+
+
+
 public class Rate {
+    private double rate;
+    private LocalDateTime timeStamp;
     
+    //Constructor for rate with past timestamp
+    public Rate(double rate, LocalDateTime time)
+    {
+        this.rate = rate;
+        this.timeStamp = time;
+    }
+    
+    //Constructor for rate with current time
+    public Rate(double rate)
+    {
+        this.rate = rate;
+        timeStamp = LocalDateTime.now();
+    }
+    
+    //Method to update rate with current time
+    public void updateRate(double rate)
+    {
+        this.rate = rate;
+        timeStamp = LocalDateTime.now();
+    }
+    
+    //Method to set rate given another rate
+    public void setRate(Rate newRate)
+    {
+        this.rate = newRate.getRate();
+        timeStamp = newRate.getTime();
+    }
+    
+    //Method to get the rate
+    public double getRate()
+    {
+        return this.rate;
+    }
+    
+    //method to get the timeStamp
+    public LocalDateTime getTime()
+    {
+        return this.timeStamp;
+    }
 }
