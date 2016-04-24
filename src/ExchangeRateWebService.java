f66d871633b151db00d2b54c13a8c39d52536d49
rcoolb2002 <rcoolb2001@yahoo.com>
2016-04-24 10:09:20 -1000

    first commit of code with webservice

---
+++ src/ExchangeRateWebService.java
@@ -0,0 +1,52 @@
+
+
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Iterator;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author rcool
+ */
+public class ExchangeRateWebService {
+    
+    private static String baseUrl = "https://finance.yahoo.com/webservice/v1/symbols/allcurrencies/quote?format=json";
+    
+    //Method returns a single exhange rate 
+    public Rate getRate(String symbol)
+    {
+        JSONParser jparse = new JSONParser();
+        Rate returnRate = null;
+        
+        try{
+            URL openURL = new URL(baseUrl);
+            URLConnection urlConnect = openURL.openConnection();
+            BufferedReader inReader = new BufferedReader(new InputStreamReader(urlConnect.getInputStream()));
+            
+            JSONObject jobj = (JSONObject)jparse.parse(inReader);
+            JSONObject list = (JSONObject) jobj.get("list");
+            JSONArray resources = (JSONArray) list.get("resources");
+            System.out.println("success");
+        } catch (IOException | ParseException e) {
+            return null;
+        }
+        return returnRate;
+    }    
+}
---
+++ src/Rate.java
@@ -0,0 +1,51 @@
+
+
+
+import java.time.LocalDateTime;
+
+
+
+public class Rate {
+    private double rate;
+    private LocalDateTime timeStamp;
+    
+    //Constructor for rate with past timestamp
+    public Rate(double rate, LocalDateTime time)
+    {
+        this.rate = rate;
+        this.timeStamp = time;
+    }
+    
+    //Constructor for rate with current time
+    public Rate(double rate)
+    {
+        this.rate = rate;
+        timeStamp = LocalDateTime.now();
+    }
+    
+    //Method to update rate with current time
+    public void updateRate(double rate)
+    {
+        this.rate = rate;
+        timeStamp = LocalDateTime.now();
+    }
+    
+    //Method to set rate given another rate
+    public void setRate(Rate newRate)
+    {
+        this.rate = newRate.getRate();
+        timeStamp = newRate.getTime();
+    }
+    
+    //Method to get the rate
+    public double getRate()
+    {
+        return this.rate;
+    }
+    
+    //method to get the timeStamp
+    public LocalDateTime getTime()
+    {
+        return this.timeStamp;
+    }
+}
