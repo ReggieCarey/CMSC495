a53d85bf5e9fdb3abaa1750f8432689887a12f13
Reginald Carey <reggie@primumgroup.com>
2016-04-24 21:13:14 -0400

    Renaming Exchange to ExchangeOLD in preparation for a replacement MAVEN based project

---
+++ ExchangeOLD/src/exchangecurrency/ExchangeRateWebService.java
@@ -0,0 +1,63 @@
+package exchangecurrency;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Iterator;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author rcool
+ */
+public class ExchangeRateWebService {
+    
+    private static final String baseUrl = "https://finance.yahoo.com/webservice/v1/symbols/allcurrencies/quote?format=json";
+    
+
+    public ArrayList<Currency> getRate()
+    {
+        JSONParser jparse = new JSONParser();
+        ArrayList<Currency> returnRates = new ArrayList();
+        
+        try{
+            URL openURL = new URL(baseUrl);
+            URLConnection urlConnect = openURL.openConnection();
+            BufferedReader inReader = new BufferedReader(new InputStreamReader(urlConnect.getInputStream()));
+            
+            JSONObject jobj = (JSONObject)jparse.parse(inReader);
+            JSONObject list = (JSONObject) jobj.get("list");
+            JSONArray resources = (JSONArray) list.get("resources");
+            
+            Iterator i = resources.iterator();
+            while(i.hasNext())
+            {
+                JSONObject j2 = (JSONObject)i.next();
+                JSONObject j3 = (JSONObject)j2.get("resource");
+                JSONObject j4 = (JSONObject)j3.get("fields");
+                String code = ((String)j4.get("symbol")).substring(0, 3);
+                double rate = Double.parseDouble((String) j4.get("price"));
+                returnRates.add(new Currency(code,rate,new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date())));
+            }
+        } catch (IOException | ParseException e) {
+            return null;
+        }
+        return returnRates;
+    }    
+}
---
+++ ExchangeOLD/src/exchangecurrency/testMain.java
@@ -0,0 +1,23 @@
+package exchangecurrency;
+
+
+import exchangecurrency.ExchangeRateWebService;
+import java.util.List;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ *
+ * @author rcool
+ */
+public class testMain {
+    public static void main(String[] args) {
+        ExchangeRateWebService webserve = new ExchangeRateWebService();
+        List testRate = webserve.getRate();
+    }
+}
