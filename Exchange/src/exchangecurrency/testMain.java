5cc0a42931b2d13a249304ab89355f67fee9938e
Reginald Carey <reggie@primumgroup.com>
2016-04-24 22:47:26 -0400

    Added code to Gui.java - added use to the testMain.java class

--- Exchange/src/exchangecurrency/testMain.java
+++ Exchange/src/exchangecurrency/testMain.java
@@ -1,14 +1,8 @@
 package exchangecurrency;
 
 
-import exchangecurrency.ExchangeRateWebService;
 import java.util.List;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 
 /**
@@ -19,5 +13,7 @@ public class testMain {
     public static void main(String[] args) {
         ExchangeRateWebService webserve = new ExchangeRateWebService();
         List testRate = webserve.getRate();
+        Gui gui = new Gui();
+        gui.startGui();
     }
 }
