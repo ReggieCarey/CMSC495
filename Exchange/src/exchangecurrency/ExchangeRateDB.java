0fc6489b0f28110be68c919c5c7cd4e371fafcaa
SirBrado <lewisindustries@gmail.com>
2016-04-25 00:28:29 -0400

    DB running and pulling info now

--- Exchange/src/exchangecurrency/ExchangeRateDB.java
+++ Exchange/src/exchangecurrency/ExchangeRateDB.java
@@ -5,10 +5,13 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 
 
@@ -31,27 +34,6 @@ public class ExchangeRateDB {
     public void updateRates(ArrayList<Currency> currentPull) {
 
         Connection conn = getConnection();
-        Currency tableCheck = new Currency();
-        currentPull = null;
-
-        //This try/catch is designed to determine if there is anything in the table.
-        try {
-            PreparedStatement ps = conn.prepareStatement("SELECT * FROM exchange WHERE rownum = 1");
-            ResultSet results = null;
-            results = ps.executeQuery();
-
-
-
-            while(results.next()) {
-
-                tableCheck.setCode(results.getString("code"));
-                tableCheck.setRate(results.getDouble("rate"));
-                tableCheck.setDate(results.getString("date"));
-
-            }
-        } catch(Exception e) {
-            e.printStackTrace();
-        }
 
         /*
             This if statement determines if there is an internet connection and cancels
@@ -62,52 +44,56 @@ public class ExchangeRateDB {
             System.out.println("No internet Connection, cannot update rates.");
 
         /*
-            This else if statement determines if the table has data in it and
-            wipes it before inserting the new data.
-        */
-        } else if(tableCheck != null){
-            try{
-            conn.createStatement().execute("DELETE * FROM exchange");
-            }catch (Exception e) {
-                e.printStackTrace();
-            }
-
-            for(int i = 1; i <= currentPull.size(); i++) {
-                try {
-                    PreparedStatement ps = conn.prepareStatement("INSERT INTO exchange VALUES "
-                            + "(?, ?, ?)");
-                    Currency tempCurrency = currentPull.get(i);
-
-                    ps.setString(1, tempCurrency.getCode());
-                    ps.setDouble(2, tempCurrency.getRate());
-                    ps.setString(3, dateFormat.format(currentDate));
-
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-
-        /*
-            This else statement recognizes that there is no current table and
-            inserts the updated information.
+            This else statement executes if there is an internet connection dropping the current table
+            and recreating the new one.
         */
         } else {
-            for(int i = 1; i <= currentPull.size(); i++) {
+            try {
+                conn.createStatement().execute("DROP TABLE exchange");
+                conn.createStatement().execute("create table exchange(code VARCHAR(4), rate DOUBLE, date VARCHAR(11))");
+                System.out.println("table created.");
+            } catch(Exception e) {
+                
+                e.printStackTrace();
+                
+            }
+
+            for(int i = 1; i < currentPull.size(); i++) {
                 try {
-                    PreparedStatement ps = conn.prepareStatement("INSERT INTO exchange VALUES "
+                    PreparedStatement ps = conn.prepareStatement("INSERT INTO exchange"
+                            + "(CODE, RATE, DATE) VALUES "
                             + "(?, ?, ?)");
                     Currency tempCurrency = currentPull.get(i);
 
                     ps.setString(1, tempCurrency.getCode());
                     ps.setDouble(2, tempCurrency.getRate());
                     ps.setString(3, dateFormat.format(currentDate));
-
+                    ps.executeUpdate();
+                    
+                    System.out.println("row add " + tempCurrency.getCode());                    
+                    
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
             }
         }
-
+        
+        try {
+            PreparedStatement prep = conn.prepareStatement("SELECT * FROM exchange");
+            ResultSet res = null;
+            int count = 1;
+            
+            res = prep.executeQuery();
+            
+            while(res.next()) {
+                System.out.println("from DB: " + res.getString("date"));
+            }
+        } catch (Exception e) {
+            
+            e.printStackTrace();
+            
+        }
+        
         //Closing the connection to the database.
         try {
             if(!conn.isClosed())
@@ -216,32 +202,20 @@ public class ExchangeRateDB {
         try {
 
             Class.forName(DRIVER);
-
+            c = DriverManager.getConnection(JDBC_URL);
+            if (c != null) {
+                System.out.println("Connected to database.");
+            }
+            
         } catch (ClassNotFoundException e){
 
             System.out.println("Where is your Derby driver?");
             e.printStackTrace();
 
+        } catch (SQLException ex) {
+            Logger.getLogger(ExchangeRateDB.class.getName()).log(Level.SEVERE, null, ex);
         }
-
-        try {
-
-            c = DriverManager.getConnection(JDBC_URL);
-
-        } catch (Exception e) {
-
-            e.printStackTrace();
-
-        }
-        try {
-
-                c.createStatement().execute("create table exchange(Code VARCHAR(4), rate DOUBLE, date VARCHAR(11)");
-
-            } catch (Exception e) {
-
-                e.printStackTrace();
-
-            }
+            
         return c;
     }
 
